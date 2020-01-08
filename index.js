d4ca7545c4cc23fbf4d27d6427c03142cba43741
Tairan Wang <tairan.wang@gmail.com>
2020-01-08 14:48:38 +0800

    :sparkles: it works

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
-const initUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/index.html";
+// 国家统计局
+const initUrl =
+  "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2018/index.html";
+
+// 国家民政部
+const url2 =
+  "http://www.mca.gov.cn/article/sj/xzqh/2019/2019/201912251506.html";
+
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(initUrl);
-    await page.screenshot({path: "dist//screenhost.png"});
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url2);
+  let areas = await page.evaluate(() => {
+    const rows = document.querySelectorAll("table > tbody > tr");
+    let data = [];
+    rows.forEach(row => {
+      var columns = row.querySelectorAll("td");
+      var node = [];
+      columns.forEach(col => {
+        if (col.innerText != "") {
+          node.push(col.innerText.trim());
+        }
+      });
+      data.push(node);
+    });
+    return data;
+  });
 
-    await browser.close();
+  var data = [];
+  areas.forEach(row => {
+    if (row[0] != null && row[1] != null) {
+      data.push([row[0], row[1]]);
+    }
+  });
+
+  data.shift(); // remote table head
+
+  fs.writeFileSync("dist/mca.china.json", JSON.stringify(data));
+
+  await browser.close();
 })();
